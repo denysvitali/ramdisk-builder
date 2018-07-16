78d3be9d612df116dc69476372908db5c44a9956
Denys Vitali <denys@denv.it>
2018-07-16 15:20:54 +0200

    Add customization

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-(cd ../smaug-custom-initram/ && find . | cpio -H newc -o | gzip -9 > /kernel/ramdisk/ramdisk.cpio.gz)
+
+usage(){
+  echo "Usage: "
+  echo "$0 initramfs-dir outfile.cpio.gz"
+}
+
+if [ -z $1 ]; then
+  usage;
+  exit 1;
+fi
+
+if [ -z $2 ]; then
+  usage;
+  exit 1;
+fi
+
+INITRAMFS_ROOT=$(readlink -f $1)
+RAMDISK_OUT=$(readlink -f $2)
+
+cd $INITRAMFS_ROOT
+find . | cpio -H newc -o | gzip -9 > $RAMDISK_OUT
+
+#(cd /kernel/smaug-custom-initram/ && find . | cpio -H newc -o | gzip -9 > /kernel/ramdisk/ramdisk.cpio.gz)
